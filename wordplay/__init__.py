5ad68cdd4000d4be3528ab6ae84e6baa44f71b9c
Martin Andrews <Martin.Andrews@PLATFORMedia.com>
2024-07-04 03:39:34 +0800

    Define Problem class

--- wordplay/__init__.py
+++ wordplay/__init__.py
@@ -100,6 +100,38 @@ def ensure_pages_downloaded(arr, site, author='teacow', polite_delay_sec=1.0):
         time.sleep(polite_delay_sec*(random.uniform(1., 3.))) # Short, variable delay
       else:
         print(f"Got code {r.status_code} for {url}")
-      
 
 
+# Process different page styles...
+has_reference = re.compile('[Ss]ee[\s\:]*\d+')
+
+class Problem:
+  num, ad, clue, pattern, answer, wordplay, comment, valid=0,'', '', '', '', '', '', True
+  def __str__(self):
+    return f"{self.num:2d}{self.ad} : {self.answer} : '{self.clue}' ({self.pattern}) :: {self.wordplay} & '{self.comment}'"
+
+  # wordplay : Capital letters get written into the grid/answer
+  # wordplay : ()* = anagrammed; (*anagram-signifier)
+  # wordplay : ()< = reversed; (<reversal-signifier)
+  # wordplay : [] = remove letters; next bracket (removal-signifier)  
+
+def get_content_from(site, fname_stub, author='teacow'):
+  page_html = f"{site['site_base']}/{author}/{fname_stub}.html"
+  fname = rel_path(f"../{page_html}")
+  with open(fname, 'rt') as f: 
+    soup = BeautifulSoup(f, features="html.parser")
+  return soup
+
+"""
+soup = wordplay.get_content_from(site, fname_stub, author='teacow')
+soup
+
+content=soup.find('div', class_='entry-content')
+content 
+
+groups =content.find_all('div', class_='fts-group')
+groups
+
+items = groups[10].find_all('div', class_='fts-subgroup')
+items
+"""
